119e13498fe1b94c53c94ad6c72091291ec6ebaa
molo06002 <molo06002@E-PW0234KQ>
2023-09-15 10:04:24 +0200

    Add project files.

---
+++ Inlämnings uppgift 2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Vilken är din favorit bok?";
+            Console.WriteLine(x);
+            string bok = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(bok);
+
+        }
+    }
+}
